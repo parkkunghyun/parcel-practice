f9018923756d6481f617aa4266fcc2871a243678
parkkunghyun <rudgus4620@gmail.com>
2021-05-11 08:59:31 +0900

    parcel

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+//nodejs는 import안씀 ESM방식말고 commomjs방식사용!!
+
+//import autoprefixer from 'autoprefixer'
+//const autoprefixer =require('autoprefixer')
+
+//export
+//plugins:[ autoprefixer]
+module.exports ={
+    plugins: [
+      require('autoprefixer')
+    ]
+}
+
